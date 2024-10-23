bdc6b70fc9183f87fba3c27422075edac8a3216d
Anders Bommo <Anders.bommo@gmail.com>
2024-10-23 14:45:00 +0200

    beste så langt!

--- WearableDevice/PositionGenerating/positionest2.py
+++ WearableDevice/PositionGenerating/positionest2.py
@@ -3,18 +3,27 @@ import pandas as pd
 from scipy.linalg import block_diag
 from pykalman import KalmanFilter
 import matplotlib.pyplot as plt
+import math
 
 # Steg 1: Les sensordata fra CSV-fil
-df = pd.read_csv('sensor_data2.csv')
+df = pd.read_csv('sensor_data3.csv')
 
 # Ekstraher nødvendige kolonner
 timestamps = df['timestamp'].values
 acc_data = df[['acc_x', 'acc_y', 'acc_z']].values
 gyro_data = df[['gyro_x', 'gyro_y', 'gyro_z']].values
+mag_data = df[['mag_x', 'mag_y', 'mag_z']].values  # Magnetometer data
 
 # Beregn tidsdifferanser mellom målinger (delta_t)
-#delta_t = np.diff(timestamps)  # Antatt at tidsstempel allerede er i sekunder
-delta_t = np.diff(timestamps) / 1000.0 
+delta_t = np.diff(timestamps)
+
+# Gravitasjon (antatt konstant langs z-aksen)
+gravity = np.array([0, 0, 9.81])
+
+# Startverdier
+initial_velocity = np.array([0, 0, 0])  # Start med null hastighet
+initial_position = np.array([0, 0, 0])  # Start med ukjent posisjon
+positions = [initial_position]  # Liste for å lagre posisjonene
 
 # Funksjon for å oppdatere posisjon fra akselerasjon
 def update_position(acc, prev_vel, prev_pos, dt):
@@ -24,27 +33,47 @@ def update_position(acc, prev_vel, prev_pos, dt):
     new_pos = prev_pos + new_vel * dt
     return new_vel, new_pos
 
-# Gravitasjon (kan justeres basert på måleforholdene)
-gravity = np.array([0, 0, 9.81])  # Standard tyngdekraft i z-retning (kan være nødvendigvis i lokale akser)
-
-# Start med null hastighet og posisjon
-initial_velocity = np.array([0, 0, 0])  # Start med null hastighet
-initial_position = np.array([0, 0, 0])  # Start med ukjent posisjon
-positions = [initial_position]  # Liste for å lagre posisjonene
-
-# Steg 2: Beregn posisjon basert på akselerometerdataene
-for i in range(1, len(acc_data)):
-    acc = acc_data[i] - gravity  # Fjern gravitasjonen fra akselerasjonen
-    dt = delta_t[i-1]
-    velocity, position = update_position(acc, initial_velocity, positions[-1], dt)
+# Funksjon for å beregne orientering fra gyroskop og magnetometer
+def update_orientation(gyro, mag, dt, prev_orientation):
+    # Beregn yaw (retningen) fra magnetometeret
+    yaw = math.atan2(mag[1], mag[0])  # Yaw fra magnetometeret (bare en enkel beregning)
     
-    # Begrens posisjon innenfor armens rekkevidde (55 cm)
-    #if np.linalg.norm(position) <= 0.55:  # 55 cm rekkevidde
-    #    positions.append(position)
-    #else:
-    #    positions.append(positions[-1])  # Behold forrige posisjon dersom vi er utenfor rekkevidden
+    # Oppdater pitch og roll fra gyroskop (integrasjon av vinkelhastighet)
+    pitch = prev_orientation[0] + gyro[0] * dt
+    roll = prev_orientation[1] + gyro[1] * dt
+    return np.array([pitch, roll, yaw])
 
+# Funksjon for å kompensere for gravitasjon basert på orientering
+def compensate_gravity(acc, orientation):
+    pitch, roll, _ = orientation  # Vi ignorerer yaw for gravitasjon
+    # Rotasjonsmatrise for å kompensere gravitasjon
+    rotation_matrix = np.array([
+        [math.cos(pitch), 0, math.sin(pitch)],
+        [0, 1, 0],
+        [-math.sin(pitch), 0, math.cos(pitch)]
+    ])
+    
+    # Kompensere gravitasjon i kroppen ved å bruke rotasjonsmatrisen
+    corrected_acc = np.dot(rotation_matrix, acc - gravity)
+    return corrected_acc
+
+# Steg 2: Beregn posisjon basert på akselerometer- og gyrodata
+orientation = np.array([0, 0, 0])  # Start med null orientering (pitch, roll, yaw)
+
+for i in range(1, len(acc_data)):
+    dt = delta_t[i-1]
+    
+    # Oppdater orienteringen basert på gyroskop og magnetometer
+    orientation = update_orientation(gyro_data[i], mag_data[i], dt, orientation)
+    
+    # Kompenser akselerasjon for gravitasjon
+    acc_corrected = compensate_gravity(acc_data[i], orientation)
+    
+    # Oppdater posisjonen basert på den korrigerte akselerasjonen
+    velocity, position = update_position(acc_corrected, initial_velocity, positions[-1], dt)
+    
     positions.append(position)
+
 positions = np.array(positions)
 
 # Steg 3: Kalman-filter for glatting
@@ -62,8 +91,8 @@ kf = KalmanFilter(
     transition_matrices=transition_matrix,
     observation_matrices=observation_matrix,
     initial_state_mean=initial_state_mean,
-    observation_covariance=0.1 * np.eye(3),  # Måling usikkerhet
-    transition_covariance=0.1 * np.eye(6)  # Systemets usikkerhet
+    observation_covariance=0.1 * np.eye(3),  # Måleusikkerhet
+    transition_covariance=0.1 * np.eye(6)    # Systemets usikkerhet
 )
 
 # Kjør Kalman-filter på posisjonsdata
@@ -80,9 +109,7 @@ filtered_df = pd.DataFrame({
     'filtered_z': filtered_positions[:, 2]
 })
 
-# Lagre som ny csv-fil
 filtered_df.to_csv('filtered_positions.csv', index=False)
-
 print("Filtrerte posisjoner er lagret i 'filtered_positions.csv'.")
 
 # Steg 5: Plot de filtrerte posisjonene i et 3D-plot
